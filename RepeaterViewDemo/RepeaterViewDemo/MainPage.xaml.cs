5e20795969b93e2f483e44f4c2e102ea3f310a19
webmasterdevlin <webmasterdevlin@gmail.com>
2018-01-28 12:29:49 +0800

    Add project files.

---
+++ RepeaterViewDemo/RepeaterViewDemo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RepeaterViewDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BindingContext = new MainViewModel();
+		}
+
+	    private void OnTap_Student(object sender, EventArgs e)
+	    {
+	        var view = sender as View;
+
+	        var employee = view?.BindingContext as string;
+
+	        DisplayAlert("Tap Event", "You have selected " + employee, "Ok");
+	    }
+
+	    private void OnTap_Employee(object sender, EventArgs e)
+	    {
+	        var view = sender as View;
+
+	        var student = view?.BindingContext as string;
+
+	        DisplayAlert("Tap Event", "You have selected " + student, "Ok");
+
+            var vm = BindingContext as MainViewModel;
+
+	        vm.SelectStudentCommand.Execute(student);
+	    }
+    }
+}
---
+++ RepeaterViewDemo/RepeaterViewDemo/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RepeaterViewDemo
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private List<string> _employees;
+        private List<string> _students;
+        public List<string> Employees
+        {
+            get => _employees;
+            set
+            {
+                _employees = value;
+                OnPropertyChanged();
+            }
+        }
+        public List<string> Students
+        {
+            get => _students;
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand SelectStudentCommand => new Command(student => {});
+        public MainViewModel()
+        {
+            Employees = new List<string>
+            {
+                "Kakashi", "Guy", "Iruka", "Asuma", "Yamato", "Minato", "Jiraiya", "Danzo", "Killer Bee", "Sarutobi", "Obito", "Orochimaru"
+            };
+
+            Students = new List<string>
+            {
+                "Naruto", "Choji", "Lee", "Sai", "Shino", "Ten ten", "Ino", "Sakura", "Hinata", "Neiji", "Sasuke", "Kiba", "Konohamaru"
+            };
+        }
+    }
+}
